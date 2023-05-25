ef438ccbece24f3b2b9a7e192deace41eae1f103
natalia185 <natalia.iwanska.ost@gmail.com>
2023-05-25 02:54:45 +0200

    wstępna wersja

--- kivy_app/app.py
+++ kivy_app/app.py
@@ -9,8 +9,12 @@ import numpy as np
 import pandas as pd
 
 
+
 class InfoWindow(Screen):
     pass
+
+
+
 class FirstWindow(Screen):
     df = None
     def selected(self, filename):
@@ -18,12 +22,17 @@ class FirstWindow(Screen):
         try:
             self.df = pd.read_csv(filename[0])
             # print(self.df)
-            self.ids.plik.text  = str(self.df.columns)
+            self.ids.plik.text  = str(self.df.columns.values)
+            self.ids.spinner_dist.values = self.df.columns.values
+            self.ids.spinner_time.values = self.df.columns.values
         except:
             try:
-                self.ids.plik.text = str( filename[0])
+                self.ids.plik.text = str(filename[0])
+                self.ids.spinner_dist.values = ''
+                self.ids.spinner_time.values = ''
             except:
                 pass
+
 
 
 class SecondWindow(Screen):
@@ -32,16 +41,19 @@ class SecondWindow(Screen):
     #     self.df = MyApp.WindowMenager.FirstWindow.df
     #     print(self.df.columns)
     pass
-
+ 
 
 
 class WindowMenager(ScreenManager):
     pass
 
+
+
 class MyApp(App):
     def build(self):
         return kv
     
+
 
 if __name__ == '__main__':
     # os.chdir(os.path.dirname(__file__))
