dc9afe2411f78277a38e3a7f6188f436381bf86d
Mateusz Machaj <mateo.mach@gmail.com>
2023-05-27 12:33:12 +0200

    basic code cleanup

--- src/app.py
+++ src/app.py
@@ -1,38 +1,33 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
-import os
-import json
-import numpy as np
 import pandas as pd
 
 
-class InfoWindow(Screen):
+class InfoScreen(Screen):
     pass
 
 
-class FirstWindow(Screen):
+class MainScreen(Screen):
     df = None
 
     def selected(self, filename):
         try:
             self.df = pd.read_csv(filename[0])
-            # print(self.df)
-            self.ids.plik.text = str(self.df.columns.values)
-            self.ids.spinner_dist.values = self.df.columns.values
+            self.ids.file.text = str(self.df.columns.values)
+            self.ids.spinner_distance.values = self.df.columns.values
             self.ids.spinner_time.values = self.df.columns.values
         except:
             try:
-                self.ids.plik.text = str(filename[0])
-                self.ids.spinner_dist.values = ""
+                self.ids.file.text = str(filename[0])
+                self.ids.spinner_distance.values = ""
                 self.ids.spinner_time.values = ""
             except:
                 pass
 
 
-class SecondWindow(Screen):
+class ResultsScreen(Screen):
     # df = None
     # def check(self):
     #     self.df = MyApp.WindowMenager.FirstWindow.df
@@ -43,8 +38,6 @@ class SecondWindow(Screen):
 class WindowManager(ScreenManager):
     pass
 
-class InfoWindow(ScreenManager):
-    pass
 
 class MyApp(App):
     def build(self):
@@ -52,8 +45,7 @@ class MyApp(App):
 
 
 if __name__ == "__main__":
-    # os.chdir(os.path.dirname(__file__))
-    print(os.getcwd(), "----------------------------------")
+    # os.chdir()
     kv = Builder.load_file("layout.kv")
-    Window.size = (1000, 800)
+    Window.size = (800, 500)
     MyApp().run()
