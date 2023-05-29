ab4d6685bf12f98c589b2a5a28e5b59610524a26
natalia185 <natalia.iwanska.ost@gmail.com>
2023-05-29 18:24:40 +0200

    logo

--- src/assets.py
+++ src/assets.py
@@ -5,7 +5,9 @@ from typing import Any, Union
 class AssetPaths:
     """Easy usage in the kivy file."""
 
-    files = {"runner_icon": "runner.png", "weight_icon": "weight.png"}
+    files = {"runner_icon": "runner.png", 
+             "weight_icon": "weight.png",
+             "logo": "karcz_run.png"}
 
     def __getitem__(self, key: Any) -> Union[Any, None]:
         try:
